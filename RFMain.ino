ed8acf57d14355c031f26421cc57572873997f80
tkaulen <t.kaulenb@gmx.de>
2016-07-24 21:57:52 +0200

    init

---
+++ RFMain.ino
@@ -0,0 +1,66 @@
+#include <Wire.h>
+
+#include "ArduinoIO.h"
+#include "HighLevelProc.h"
+
+
+#include "CC1001.h"
+
+char inputBuffer[512];
+
+void onRadioConfig(int radioID, char txrx, int protocolNr, int modulationType,  long frequency,  long bandwidth,  long drate,  long fhub, char changeRadioID, char changeTxrx, char changeProtocolNr, char changeModulationType,  char changeFrequency,  char changeBandwidth,  char changeDrate,   char changeFhub)
+{
+  
+  Serial.println(txrx,DEC); 
+  if (changeTxrx)
+  {
+    
+      if (txrx  == radioTX) setIOTX();  
+      if (txrx  == radioRX) setIORX(); 
+      if (txrx  == radioIDLE) setIOTX();           
+  }
+  ccConfigAll(radioID,  txrx,  protocolNr, modulationType,   frequency,   bandwidth,   drate,   fhub,
+ changeRadioID,  changeTxrx,  changeProtocolNr,  changeModulationType,   changeFrequency,   changeBandwidth,   changeDrate,    changeFhub);
+
+}
+
+
+void setup() {
+
+  //Serial.begin(115200);
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  // put your setup code here, to run once:
+ 
+ initArduinoIO();
+ setHighLevelCallback(onSendTTL, onReadSymbol,onRadioConfig);
+ setProtocol(protIntertechno);
+ updateRadio();
+ //parseCommandLine("> P=2 {10f0000000f0}");
+}
+
+
+void loop() {
+  while(true)
+  {
+    digitalWrite(35,HIGH);  digitalWrite(35,LOW);
+    
+ //   unsigned int  t = TCNT1;
+ //   TCNT1 =0;
+  //    decodeTickDeltaTime(t,getRXLevel());
+//  decodeTickAbsoluteTime(micros(),getRXLevel());
+
+   char chRead = Serial.read(); 
+ 
+   if (chRead != -1)
+  {
+   
+    /// Serial.write(chRead);
+    readLine(chRead, inputBuffer, 200);
+    }
+   
+  }
+   
+}
